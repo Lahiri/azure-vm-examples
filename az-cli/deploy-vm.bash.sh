c798e4f8d8db7f79f13d74c324126fac24270599
Lahiri Cristofori <crlahiri@microsoft.com>
2020-03-23 17:31:06 +0100

    removed one-liners

--- az-cli/deploy-vm.bash.sh
+++ az-cli/deploy-vm.bash.sh
@@ -14,6 +14,5 @@ az vm create \
     --subnet Example-Subnet \
     --admin-username azure_user \
     --tags OS=Windows Application=Example \
+    #--admin-password <secret-string> \
 /
-# one-liner
-# az vm create --name Example-VM --resource-group Example-RG --location westeurope --zone 1 --image win2016datacenter --size Standard_DS2_v2 --public-ip-address "" --nsg "" --license-type Windows_Server --storage-sku StandardSSD_LRS --data-disk-sizes-gb 128 --vnet-name Example-Vnet --subnet Example-Subnet --admin-username azure_user --tags OS=Windows Application=Example
